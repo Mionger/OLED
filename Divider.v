6f9dc51f3e3ebe17c8d13db6d031a52260a08142
Park Mion-ger <parkmionger@tongji.edu.cn>
2019-02-19 21:07:34 +0800

    Add 1 file Divider.v

---
+++ Divider.v
@@ -0,0 +1,20 @@
+module  Divider
+(
+    DI,
+    DO,
+    N
+);
+    input DI;
+    output reg DO=0;
+
+    integer t_cnt=0;
+    always @(posedge DI)begin
+        if(t_cnt<N/2-1) begin
+            t_cnt<=t_cnt+1;
+        end
+        else begin
+            t_cnt<=0;
+            DO<=~DO;
+        end
+    end
+endmodule
