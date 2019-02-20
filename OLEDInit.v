0096771ad1799ffa5886ba7c02e3609dd6f05102
Park Mion-ger <parkmionger@tongji.edu.cn>
2019-02-20 10:46:45 +0800

    Complete OLEDInit.v and wait for simulation to debug

--- OLEDInit.v
+++ OLEDInit.v
@@ -4,8 +4,8 @@ module OLED_Init
     RST_N,
     START,          //initial start
     DONE,           //initial done
-    WRITE_START,    //spi write start
-    WRITE_DONE,     //spi write done
+    SPI_START,      //spi write start
+    SPI_DONE,       //spi write done
     DATA,           //spi data
     RST_OLED        //hard interface reset
 );
@@ -13,10 +13,10 @@ module OLED_Init
     input CLK;
     input RST_N;
     input START;
-    input WRITE_DONE;
+    input SPI_DONE;
 
     output DONE;
-    output WRITE_START;
+    output SPI_START;
     output [9:0]DATA;
     output RST_OLED;
 
@@ -45,29 +45,45 @@ module OLED_Init
     end
 
     //initialize
-    parameter DISPLAY_OFF                     = 8'd0;
-    parameter SET_CONTRAST_A                  = 8'd1;//blue
-    parameter SET_CONTRAST_A_ARGUMENT         = 8'd2;
-    parameter SET_CONTRAST_B                  = 8'd3;//green
-    parameter SET_CONTRAST_B_ARGUMENT         = 8'd4;
-    parameter SET_CONTRAST_C                  = 8'd5;//red
-    parameter SET_CONTRAST_C_ARGUMENT         = 8'd6;
-    parameter MASTER_CURRENT_CONTROL          = 8'd7;
-    parameter MASTER_CURRENT_CONTROL_ARGUMENT = 8'd8;
-    parameter SET_PRECHARGE_SPEED_A           = 8'd9;
-    parameter SET_PRECHARGE_SPEED_A_ARGUMENT  = 8'd10;
-    parameter SET_PRECHARGE_SPEED_B           = 8'd11;
-    parameter SET_PRECHARGE_SPEED_B_ARGUMENT  = 8'd12;
-    parameter SET_PRECHARGE_SPEED_C           = 8'd13;
-    parameter SET_PRECHARGE_SPEED_C_ARGUMENT  = 8'd14;
-    parameter SET_REMAP                       = 8'd15;
-    parameter SET_REMAP_ARGUMENT              = 8'd16;
-    parameter SET_DISPLAY_START_LINE          = 8'd17;
-    parameter SET_DISPLAY_START_LINE_ARGUMENT = 8'd18;
-    parameter SET_DISPLAY_OFFSET              = 8'd19;
-    parameter SET_DISPLAY_OFFSET_ARGUMENT     = 8'd20;
-    parameter NORMAL_DISPLAY                  = 8'd21;
-    reg [7:0]state;
+    parameter DISPLAY_OFF                        = 8'd0;
+    parameter SET_CONTRAST_A                     = 8'd1;//blue
+    parameter SET_CONTRAST_A_ARGUMENT            = 8'd2;
+    parameter SET_CONTRAST_B                     = 8'd3;//green
+    parameter SET_CONTRAST_B_ARGUMENT            = 8'd4;
+    parameter SET_CONTRAST_C                     = 8'd5;//red
+    parameter SET_CONTRAST_C_ARGUMENT            = 8'd6;
+    parameter MASTER_CURRENT_CONTROL             = 8'd7;
+    parameter MASTER_CURRENT_CONTROL_ARGUMENT    = 8'd8;
+    parameter SET_PRECHARGE_SPEED_A              = 8'd9;
+    parameter SET_PRECHARGE_SPEED_A_ARGUMENT     = 8'd10;
+    parameter SET_PRECHARGE_SPEED_B              = 8'd11;
+    parameter SET_PRECHARGE_SPEED_B_ARGUMENT     = 8'd12;
+    parameter SET_PRECHARGE_SPEED_C              = 8'd13;
+    parameter SET_PRECHARGE_SPEED_C_ARGUMENT     = 8'd14;
+    parameter SET_REMAP                          = 8'd15;
+    parameter SET_REMAP_ARGUMENT                 = 8'd16;
+    parameter SET_DISPLAY_START_LINE             = 8'd17;
+    parameter SET_DISPLAY_START_LINE_ARGUMENT    = 8'd18;
+    parameter SET_DISPLAY_OFFSET                 = 8'd19;
+    parameter SET_DISPLAY_OFFSET_ARGUMENT        = 8'd20;
+    parameter NORMAL_DISPLAY                     = 8'd21;
+    parameter SET_MULTIPLEX_RATIO                = 8'd22;
+    parameter SET_MULTIPLEX_RATIO_ARGUMENT       = 8'd23;
+    parameter SET_MASTER_CONFIGURATION           = 8'd24;
+    parameter SET_MASTER_CONFIGURATION_ARGUMENT  = 8'd25;
+    parameter POWER_SAVE_MODE                    = 8'd26;
+    parameter POWER_SAVE_MODE_ARGUMENT           = 8'd27;
+    parameter PHASE_PERIOD_ADJUSTMENT            = 8'd28;
+    parameter PHASE_PERIOD_ADJUSTMENT_ARGUMENT   = 8'd29;
+    parameter DISPLAY_CLOCK_DIV                  = 8'd30;
+    parameter DISPLAY_CLOCK_DIV_ARGUMENT         = 8'd31;
+    parameter SET_PRECHARGE_VOLTAGE              = 8'd32;
+    parameter SET_PRECHARGE_VOLTAGE_ARGUMENT     = 8'd33;
+    parameter SET_V_VOLTAGE                      = 8'd34;
+    parameter SET_V_VOLTAGE_ARGUMENT             = 8'd35;
+    parameter DISPLAY_ON                         = 8'd36;
+    
+    reg [7:0]state = DISPLAY_OFF;
     reg [9:0]data;
     reg start;
     reg done;
@@ -81,7 +97,7 @@ module OLED_Init
         else if (START & rst_done) begin
             case (state)
                 DISPLAY_OFF:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -91,7 +107,7 @@ module OLED_Init
                     end
                 end
                 SET_CONTRAST_A:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -101,7 +117,7 @@ module OLED_Init
                     end
                 end
                 SET_CONTRAST_A_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -111,7 +127,7 @@ module OLED_Init
                     end
                 end
                 SET_CONTRAST_B:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -121,7 +137,7 @@ module OLED_Init
                     end
                 end
                 SET_CONTRAST_B_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -131,7 +147,7 @@ module OLED_Init
                     end
                 end  
                 SET_CONTRAST_C:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -141,7 +157,7 @@ module OLED_Init
                     end
                 end
                 SET_CONTRAST_C_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -151,7 +167,7 @@ module OLED_Init
                     end
                 end
                 MASTER_CURRENT_CONTROL:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -161,7 +177,7 @@ module OLED_Init
                     end
                 end
                 MASTER_CURRENT_CONTROL_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -171,7 +187,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_A:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -181,7 +197,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_A_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -191,7 +207,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_B:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -201,7 +217,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_B_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -211,7 +227,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_C:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -221,7 +237,7 @@ module OLED_Init
                     end
                 end
                 SET_PRECHARGE_SPEED_C_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -231,7 +247,7 @@ module OLED_Init
                     end
                 end
                 SET_REMAP:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -241,7 +257,7 @@ module OLED_Init
                     end
                 end
                 SET_REMAP_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -251,7 +267,7 @@ module OLED_Init
                     end
                 end
                 SET_DISPLAY_START_LINE:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -261,7 +277,7 @@ module OLED_Init
                     end
                 end
                 SET_DISPLAY_START_LINE_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -271,7 +287,7 @@ module OLED_Init
                     end
                 end
                 SET_DISPLAY_OFFSET:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -281,7 +297,7 @@ module OLED_Init
                     end
                 end
                 SET_DISPLAY_OFFSET_ARGUMENT:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -291,7 +307,7 @@ module OLED_Init
                     end
                 end
                 NORMAL_DISPLAY:begin
-                    if(WRITE_DONE) begin
+                    if(SPI_DONE) begin
                         state <= state + 1'b1;
                         start <= 1'b0;
                     end
@@ -300,13 +316,162 @@ module OLED_Init
                         start <= 1'b1;
                     end
                 end
-                default: 
+                SET_MULTIPLEX_RATIO:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hA8};//command code
+                        start <= 1'b1;
+                    end
+                end
+                SET_MULTIPLEX_RATIO_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h3F};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                SET_MASTER_CONFIGURATION:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hAD};//command code
+                        start <= 1'b1;
+                    end
+                end
+                SET_MASTER_CONFIGURATION_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h8E};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                POWER_SAVE_MODE:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hB0};//command code
+                        start <= 1'b1;
+                    end
+                end
+                POWER_SAVE_MODE_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h00};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                PHASE_PERIOD_ADJUSTMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hB1};//command code
+                        start <= 1'b1;
+                    end
+                end
+                PHASE_PERIOD_ADJUSTMENT_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h31};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                DISPLAY_CLOCK_DIV:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hB3};//command code
+                        start <= 1'b1;
+                    end
+                end
+                DISPLAY_CLOCK_DIV_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hF0};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                SET_PRECHARGE_VOLTAGE:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hBB};//command code
+                        start <= 1'b1;
+                    end
+                end
+                SET_PRECHARGE_VOLTAGE_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h3A};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                SET_V_VOLTAGE:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hBE};//command code
+                        start <= 1'b1;
+                    end
+                end
+                SET_V_VOLTAGE_ARGUMENT:begin
+                    if(SPI_DONE) begin
+                        state <= state + 1'b1;
+                        start <= 1'b0;
+                    end
+                    else begin
+                        data  <= {2'b00,8'h3E};//command argument
+                        start <= 1'b1;
+                    end
+                end
+                default:begin
+                    if(SPI_DONE) begin
+                        start <= 1'b0;
+                        done  <= 1'b1;
+                    end
+                    else begin
+                        data  <= {2'b00,8'hAF};//command argument
+                        start <= 1'b1;
+                    end
+                end
             endcase
         end
     end
 
     assign  DATA = data;
-    assign  WRITE_START = start;
+    assign  SPI_START = start;
     assign  DONE = done;
 endmodule
 
