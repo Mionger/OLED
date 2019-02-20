a90fba73e0f9de75583af318a6e9a92483176f5e
Park Mion-ger <parkmionger@tongji.edu.cn>
2019-02-21 00:39:05 +0800

    Transform passing argument into function called argument

--- Divider.v
+++ Divider.v
@@ -1,18 +1,17 @@
-module  Divider
+module Divider #(parameter N=100000)
 (
     DI,
-    DO,
-    N
+    DO
 );
     input DI;
-    input N;
+    // input [2:0]N;
 
     output DO;
 
     reg DO = 0;
 
     integer t_cnt = 0;
-    always @(posedge DI) begin
+    always @(posedge DI)begin
         if(t_cnt<N/2-1) begin
             t_cnt <= t_cnt + 1;
         end
